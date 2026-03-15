529d42135a91e2c4d68ac1f8115ad3b17fcdf595
Souyerliu <zsy142857@126.com>
2026-03-15 17:34:52 +0800

    post-update-3

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -143,7 +143,7 @@ export default defineConfig({
     },
   },
   nyxPlayer: {
-    enable: true,
+    enable: false,
     preset: "shokax",
     darkModeTarget: ':root[data-theme="dark"]',
     urls: [
